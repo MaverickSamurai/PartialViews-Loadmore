9e6c5a29a7bfe25b4124dec50bf6c3f298a2b9a5
MaverickSamurai <mirsamirba@code.edu.az>
2022-10-12 02:47:48 +0400

    PartialViews-Loadmore

---
+++ PartialViews-Loadmore/PartialViews-Loadmore/Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PartialViews_Loadmore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PartialViews_Loadmore.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ PartialViews-Loadmore/PartialViews-Loadmore/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PartialViews_Loadmore.Models
+{
+    public class Product : BaseEntity
+    {
+        public string Image { get; set; }
+        public string Title { get; set; }
+
+        [Column(TypeName = "decimal(14,4)")]
+        public decimal Price { get; set; }
+    }
+}
